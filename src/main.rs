c39304c2d0f39632ccaa95aed8836eee22846d3d
Ryan Ruckley <rruckley@gmail.com>
2023-09-27 15:27:12 +1000

    Initial Leptos code

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button on:click=move |_| {
+            set_count.update(|n| *n += 1);
+        }>"Click me: "
+        {count}
+        </button>
+    }
+}
+
+fn main() {
+    mount_to_body(|| view! {<App/>})
+}
