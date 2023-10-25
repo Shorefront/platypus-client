e0877343495db033cc709f2c242267521eed9e49
Ryan Ruckley <rruckley@gmail.com>
2023-10-25 18:15:07 +1100

    working

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,96 @@
 use leptos::*;
+use leptos_router::*;
 
 use tmflib::tmf629::customer::Customer;
 use tmflib::tmf632::organization::Organization;
 
 #[component]
-fn App() -> impl IntoView {
-
-    let org = Organization::new("My Org".to_owned());
-    let c1 = Customer::new(org);
-
-    let customers = create_rw_signal(vec![c1]);
-
-    let (count, set_count) = create_signal(0);
-
+fn CustomerRow(customer : Customer) -> impl IntoView {
     view! {
-        <button on:click=move |_| {
-            set_count.update(|n| *n += 1);
-        }>"Click this: "
-        {count}
-        </button>
+        <tr><td><a href={customer.href}>{ customer.id}</a></td><td>{ customer.name }</td></tr>
+    }
+}
+
+#[component]
+fn IndividualTable() -> impl IntoView {
+    view! {
+        <div>
+        <h2>Individual Table</h2>
+        <table>
+            <tr>
+                <th>Name</th>
+            </tr>
+        </table>
+        </div>
+    }
+}
+
+#[component]
+fn CustomerTable() -> impl IntoView {
+    let org = Organization::new("Another Org".to_owned());
+    let c1 = Customer::new(org);
+    let org2 = Organization::new("Mangus Org".to_owned());
+    let c2 = Customer::new(org2);
+    let customers = vec![c1,c2];
+    view! {
+        <div>
+        <h2>Customer Table</h2>
+            <table>
+                <tr>
+                    <th>Id</th><th>Customer</th>
+                </tr>
+         
+                {customers.into_iter()
+                    .map(|c| {
+                        CustomerRow(CustomerRowProps{ customer: c})
+                    }).collect_view()
+                }
+            </table>
+        </div>
+    }
+}
+
+#[component]
+fn Menu() -> impl IntoView {
+    view!{
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/tmflib/tmf629/customer">"Party / Customer"</a></li>
+            <li><a href="/tmflib/tmf632/individual">"Party / Individual"</a></li>
+        </ul>
+    }
+}
+
+#[component]
+fn Home() -> impl IntoView {
+    view!{
+        <div>
+        <h2>TMF Data Management</h2>
+        <p>This is an experiemental TMF data management platform written in Rust.</p>
+        </div>
+    }
+}
+
+#[component]
+fn App1() -> impl IntoView {
+
+    view!{
+        <Router>
+            <h1>"Platypus - TMF Management"</h1>
+            <nav>
+                {Menu()}
+            </nav>
+            <main>
+                <Routes>
+                    <Route path="/" view=Home/>
+                    <Route path="/tmflib/tmf629/customer" view=CustomerTable/>
+                    <Route path="/tmflib/tmf632/individual" view=IndividualTable/>
+                </Routes>
+            </main>
+        </Router>
     }
 }
 
 fn main() {
-    mount_to_body(|| view! {<App/>})
+    leptos::mount_to_body(App1)
 }
