fe50bc40ad5345f183de46e45fad63debbb75b05
Ryan Ruckley <rruckley@gmail.com>
2023-10-26 12:56:37 +1100

    new mod

---
+++ src/model/tmf620/catalog.rs
@@ -0,0 +1,17 @@
+use leptos::*;
+
+
+#[component]
+pub fn CatalogTable() -> impl IntoView {
+    view! {
+
+    }
+}
+
+#[component]
+pub fn CatalogView(id : String) -> impl IntoView {
+    view! {
+        <svg>
+        </svg>
+    }
+}
