dbb178d9be66002e21bc59ee8cd477d11bdb1db7
Ryan Ruckley <rruckley@gmail.com>
2023-10-30 10:16:18 +1100

    new view

---
+++ src/model/tmf629/mod.rs
@@ -0,0 +1,13 @@
+//! Customer components
+
+
+use leptos::*;
+
+#[component]
+pub fn CustomerView() -> impl IntoView {
+    view! {
+        <h2>"Customer Details"</h2>
+        <table>
+        </table>
+    }
+}
