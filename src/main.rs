5634a99a2e92f6fa165bb4ba5926d7d6527ce446
Ryan Ruckley <rruckley@gmail.com>
2023-12-19 09:06:16 +1100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use leptos::*;
 use leptos_router::*;
 
-use tmflib::tmf629::customer::Customer;
-use tmflib::tmf632::organization::Organization;
-
 mod model;
 
 use model::tmf620::CatalogRoutes;
@@ -13,25 +10,6 @@ use model::tmf648::QuoteRoutes;
 use model::common::{Menu,GenericTable};
 
 #[warn(missing_docs)]
-
-#[component]
-fn CustomerRow(customer : Customer) -> impl IntoView {
-    view! {
-        <tr><td><a href={customer.href}>{ customer.id}</a></td><td>{ customer.name }</td></tr>
-    }
-}
-
-#[component]
-fn CustomerTable() -> impl IntoView {
-    let org = Organization::new("Another Org".to_owned());
-    let c1 = Customer::new(org);
-    let org2 = Organization::new("Mangus Org".to_owned());
-    let c2 = Customer::new(org2);
-    let customers = vec![c1,c2];
-    view! {
-        <GenericTable items=customers/>
-    }
-}
 
 #[component]
 fn Home() -> impl IntoView {
