9e82aa4407a82afa3eef8e666af71449dc463c03
Ryan Ruckley <rruckley@gmail.com>
2023-12-18 16:11:34 +1100

    Detail component

--- src/model/tmf620/product_offering.rs
+++ src/model/tmf620/product_offering.rs
@@ -1,13 +1,29 @@
 //! Product Offering Model
 
 use leptos::*;
+use leptos_router::Outlet;
+use crate::model::common::GenericTable;
 
+use tmflib::tmf620::product_offering::ProductOffering;
 
 #[component]
 pub fn ProductOfferingTable() -> impl IntoView {
+    let offer1 = ProductOffering::new("Offer1".to_string());
+    let items = vec![offer1];
     view! {
-        <p>"Product Offering Table"</p>
-        <table>
-        </table>
+        <div class="list">
+            
+            <GenericTable items=items/>
+        </div>
+        <div class="detail">
+            <Outlet />
+        </div>
+    }
+}
+
+#[component]
+pub fn ProductOfferingView() -> impl IntoView {
+    view! {
+        <p>"Product Offering View"</p>
     }
 }
