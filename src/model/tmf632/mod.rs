94246791785f3adbc2240ae74e1f79d78487fcb4
Ryan Ruckley <rruckley@gmail.com>
2023-12-18 13:14:34 +1100

    party mgt components / routes

--- src/model/tmf632/mod.rs
+++ src/model/tmf632/mod.rs
@@ -1,3 +1,43 @@
 
 pub mod individual;
 pub mod organization;
+
+use leptos_router::*;
+use leptos::*;
+
+use organization::{OrganizationTable,OrganizationView};
+use individual::{IndividualTable,IndividualView};
+
+#[component(transparent)]
+pub fn PartyRoutes() -> impl IntoView {
+    view! {
+        <Route path="/tmf-api/tmf632/v4" view=PartyHome>
+            <Route path="organisation" view=OrganizationTable >
+                <Route path=":id" view=OrganizationView />
+            </Route>
+            <Route path="individual" view=IndividualTable >
+                <Route path=":id" view=IndividualView />
+            </Route>
+            <Route path="" view=|| {
+                view! {
+                    <p>"Please select something"</p>
+                }
+            } />
+        </Route>
+    }
+}
+
+#[component]
+pub fn PartyHome() -> impl IntoView {
+    view! {
+        <nav>
+            <ul class="menu">
+                <li><a href="/tmf-api/tmf632/v4/organization">"Organisation"</a></li>
+                <li><a href="/tmf-api/tmf632/v4/individual">"Individual"</a></li>
+            </ul>
+        </nav>
+        <div>
+        <Outlet />
+        </div>
+    }
+}
