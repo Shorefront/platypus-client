4d0d833447b633c58ba1daa8e4392c989ee98ef5
Ryan Ruckley <rruckley@gmail.com>
2023-12-18 16:11:07 +1100

    Consistent routes

--- src/model/tmf620/mod.rs
+++ src/model/tmf620/mod.rs
@@ -3,26 +3,49 @@ pub mod catalog;
 pub mod category;
 pub mod product_specification;
 pub mod product_offering;
+pub mod product_offering_price;
 
 use leptos::*;
 use leptos_router::*;
 
 use catalog::{CatalogTable,CatalogView};
-use category::CategoryTable;
+use category::{CategoryTable,CategoryView};
+use product_offering::{ProductOfferingTable,ProductOfferingView};
+use product_specification::{ProductSpecificationTable,ProductSpecificationView};
+use product_offering_price::{ProductOfferingPriceList,ProductOfferingPriceDetail};
+
+#[component]
+pub fn NoOptionView() -> impl IntoView {
+    view! {
+        <p>"Please select an option"</p>
+    }
+}
 
 #[component(transparent)]
 pub fn CatalogRoutes() -> impl IntoView {
     view! {
-        <Route path="/tmf-api/productCatalogManagement/v4/" view=CatalogHome>
+        <Route path="/tmf-api/productCatalogManagement/v4" view=CatalogHome>
             <Route path="catalog" view=CatalogTable >
-                <Route path=":id" view=CatalogView />
+                <Route path=":id" view=CatalogView /> 
+                <Route path="" view=NoOptionView />
             </Route>
-            <Route path="category" view=CategoryTable />
-            <Route path="" view=|| {
-                view! {
-                    <p>"Please select something"</p>
-                }
-            } />
+            <Route path="category" view=CategoryTable >
+                <Route path=":id" view=CategoryView />
+                <Route path="" view=NoOptionView />
+            </Route>
+            <Route path="productOffering" view=ProductOfferingTable >
+                <Route path=":id" view=ProductOfferingView />
+                <Route path="" view=NoOptionView />
+            </Route>
+            <Route path="productSpecification" view=ProductSpecificationTable >
+                <Route path=":id" view=ProductSpecificationView />
+                <Route path="" view=NoOptionView />
+            </Route>
+            <Route path="productOfferingPrice" view=ProductOfferingPriceList >
+                <Route path=":id" view=ProductOfferingPriceDetail />
+                <Route path="" view=NoOptionView />
+            </Route>
+            <Route path="" view=NoOptionView />
         </Route>
     }
 }
@@ -34,9 +57,9 @@ pub fn CatalogHome() -> impl IntoView {
             <ul class="menu">
                 <li><a href="/tmf-api/productCatalogManagement/v4/catalog">"Catalog"</a></li>
                 <li><a href="/tmf-api/productCatalogManagement/v4/category">"Category"</a></li>
-                <li>Offering</li>
-                <li>Specification</li>
-                <li>Pricing</li>
+                <li><a href="/tmf-api/productCatalogManagement/v4/productOffering">"Offering"</a></li>
+                <li><a href="/tmf-api/productCatalogManagement/v4/productSpecification">"Specification"</a></li>
+                <li><a href="/tmf-api/productCatalogManagement/v4/productOfferingPrice">"Pricing"</a></li>
             </ul>
         </nav>
 
