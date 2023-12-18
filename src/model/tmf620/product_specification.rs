118925b51c0059ac4ab00b88191c22167d587d1e
Ryan Ruckley <rruckley@gmail.com>
2023-12-18 16:10:52 +1100

    New Components

--- src/model/tmf620/product_specification.rs
+++ src/model/tmf620/product_specification.rs
@@ -1,12 +1,28 @@
 //! Product Specification Model
 
 use leptos::*;
+use leptos_router::Outlet;
+
+use crate::model::common::GenericTable;
+use tmflib::tmf620::product_specification::ProductSpecification;
 
 #[component]
 pub fn ProductSpecificationTable() -> impl IntoView {
+    let spec1 = ProductSpecification::new("Spec One".to_string());
+    let specs = vec![spec1];
     view! {
-        <p>"A Product Specification Table"</p>
-        <table>
-        </table>
+        <div class="list">
+            <GenericTable items=specs />
+        </div>
+        <div class="detail">
+            <Outlet />
+        </div>
+    }
+}
+
+#[component]
+pub fn ProductSpecificationView() -> impl IntoView {
+    view! {
+        <p>"A view of specifications"</p>
     }
 }
