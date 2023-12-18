30e19b670496249a9dc9139565c2bd8b7e0954c6
Ryan Ruckley <rruckley@gmail.com>
2023-12-18 12:11:45 +1100

    move menu items into mods

--- src/model/common.rs
+++ src/model/common.rs
@@ -15,7 +15,7 @@ fn TableRow<T : HasId + HasName>(item : T) -> impl IntoView {
 pub fn GenericTable<T : HasId + HasName>(items : Vec<T>) -> impl IntoView {
     view! {
         <div style="float: left; ">
-        <h2>{T::get_class().to_uppercase()} List</h2>
+        <h2>{T::get_class().to_uppercase()}</h2>
             <table style="border: 1px solid;width: 30%;">
                 <tr>
                     <th>{ T::get_class()}</th>
@@ -34,29 +34,16 @@ pub fn GenericTable<T : HasId + HasName>(items : Vec<T>) -> impl IntoView {
 #[component]
 pub fn Menu() -> impl IntoView {
     view!{
-        <ul>
+        <ul class="menu">
             <li><a href="/">"Home"</a></li>
             <li>
-                "TMF620 Product Catalog"
-                <ul>
-                    <li><a href="/tmflib/productCatalogManagement/v4/category">"Categories"</a></li>
-                    <li><a href="/tmflib/productCatalogManagement/v4/catalog">"Catalog"</a></li>
-                    <li><a href="/tmflib/productCatalogManagement/v4/productOffering">"Product Offers"</a></li>
-                    <li><a href="/tmflib/productCatalogManagement/v4/productSpecification">"Product Specification"</a></li>
-                </ul>
+                <a href="/tmf-api/productCatalogManagement/v4">"TMF620"</a>
             </li>
             <li>
-            "TMF629"
-                <ul>
-                    <li><a href="/tmf-api/tmf629/v4/customer">"Customer"</a></li>
-                </ul>
+                "TMF629"
             </li>
             <li>
-                "TMF632 Party"
-                <ul>
-                    <li><a href="/tmf-api/tmf632/v4/individual">"Individual"</a></li>    
-                    <li><a href="/tmf-api/tmf632/v4/organization">"Organization"</a></li>
-                </ul>
+                "TMF632"
             </li>
         </ul>
     }
