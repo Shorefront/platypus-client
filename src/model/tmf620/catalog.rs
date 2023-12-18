85cde0eb98389b063fdcdc026f237b9aeb4ecb38
Ryan Ruckley <rruckley@gmail.com>
2023-12-18 12:11:16 +1100

    Use outlet Additional structure

--- src/model/tmf620/catalog.rs
+++ src/model/tmf620/catalog.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use leptos_router::*;
 
 use tmflib::tmf620::catalog::Catalog;
 use super::super::common::GenericTable;
@@ -9,13 +10,18 @@ pub fn CatalogTable() -> impl IntoView {
     let cat2 = Catalog::new("Run");
     let catalogs = vec![cat1,cat2];
     view! {
-        <GenericTable items=catalogs/>
+        <div class="list">
+            <GenericTable items=catalogs/>
+        </div>
+        <div class="detail">
+            <Outlet />
+        </div>
     }
 }
 
 #[component]
 pub fn CatalogView() -> impl IntoView {
     view! {
-        <p>"Some data"</p>
+        <p>"Catalog Item"</p>
     }
 }
