a37c93e3ee488cce7372160439c2c261701030a8
Ryan Ruckley <rruckley@gmail.com>
2023-12-18 12:10:12 +1100

    move routes into mod

--- src/model/tmf620/mod.rs
+++ src/model/tmf620/mod.rs
@@ -3,3 +3,43 @@ pub mod catalog;
 pub mod category;
 pub mod product_specification;
 pub mod product_offering;
+
+use leptos::*;
+use leptos_router::*;
+
+use catalog::{CatalogTable,CatalogView};
+use category::CategoryTable;
+
+#[component(transparent)]
+pub fn CatalogRoutes() -> impl IntoView {
+    view! {
+        <Route path="/tmf-api/productCatalogManagement/v4" view=CatalogHome>
+            <Route path="catalog" view=CatalogTable >
+                <Route path=":id" view=CatalogView />
+            </Route>
+            <Route path="category" view=CategoryTable />
+            <Route path="" view=|| {
+                view! {
+                    <p>"Please select something"</p>
+                }
+            } />
+        </Route>
+    }
+}
+
+#[component]
+pub fn CatalogHome() -> impl IntoView {
+    view! {
+        <nav>
+            <ul class="menu">
+                <li><a href="catalog">"Catalog"</a></li>
+                <li><a href="category">"Category"</a></li>
+                <li>Offering</li>
+                <li>Specification</li>
+                <li>Pricing</li>
+            </ul>
+        </nav>
+
+        <Outlet />
+    }
+}
