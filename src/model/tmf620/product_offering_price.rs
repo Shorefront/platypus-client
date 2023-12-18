da0b8d82f8a3df09b74e8c9c35fff12520fb7157
Ryan Ruckley <rruckley@gmail.com>
2023-12-18 16:11:18 +1100

    List / Detail components

---
+++ src/model/tmf620/product_offering_price.rs
@@ -0,0 +1,28 @@
+//! Product Offering Price Module
+
+use tmflib::tmf620::product_offering_price::ProductOfferingPrice;
+use crate::model::common::GenericTable;
+
+use leptos::*;
+use leptos_router::*;
+
+#[component]
+pub fn ProductOfferingPriceList() -> impl IntoView {
+    let price1 = ProductOfferingPrice::new("Price1".to_string());
+    let prices = vec![price1];
+    view! {
+        <div class="list">
+            <GenericTable items=prices />
+        </div>
+        <div class="detail">
+            <Outlet />
+        </div>
+    }
+}
+
+#[component]
+pub fn ProductOfferingPriceDetail() -> impl IntoView {
+    view! {
+        <p>"Product Offering Price"</p>
+    }
+}
