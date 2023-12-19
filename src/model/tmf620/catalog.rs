5f7a6b30acb2be2827dea0c3e6ab97916e82c073
Ryan Ruckley <rruckley@gmail.com>
2023-12-19 15:07:48 +1100

    revert

--- src/model/tmf620/catalog.rs
+++ src/model/tmf620/catalog.rs
@@ -4,20 +4,11 @@ use leptos_router::*;
 use tmflib::tmf620::catalog::Catalog;
 use super::super::common::GenericTable;
 
-async fn load_catalogs() -> Vec<Catalog> {
-    // Call Platypus API to retrieve Catalogs
-    let cat1 = Catalog::new("Design");
-    let cat2 = Catalog::new("Run");
-    let catalogs = vec![cat1,cat2];
-    catalogs
-}
-
 #[component]
 pub fn CatalogList() -> impl IntoView {
-    let stable = create_resource(|| (), |_| async move {
-        load_catalogs().await
-    });
-    let catalogs = stable.get().unwrap_or_default();
+    let cat1 = Catalog::new("Design");
+    let cat2 = Catalog::new("Production");
+    let catalogs = vec![cat1,cat2];
     view! {
         <div class="list">
             <GenericTable items=catalogs />
