c5099d29ff4f0e4094732a147838a24e7b2e95b8
Ryan <rruckley@gmail.com>
2024-01-05 19:56:49 +1100

    use module routes

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use leptos_router::*;
 
 mod model;
 
+// Routes
 use model::tmf620::CatalogRoutes;
-use model::tmf629::CustomerRoutes;
-use model::tmf632::PartyRoutes;
-use model::tmf648::QuoteRoutes;
+
+// Common
 use model::common::{Banner,Menu,GenericTable};
 
 #[warn(missing_docs)]
@@ -48,16 +48,8 @@ fn Platypus() -> impl IntoView {
             </nav>
             <main>
                 <Routes>
+                    <CatalogRoutes />
                     <Route path="/" view=Home/>
-                    <Route path="/tmflib/productCatalogManagement/catalog" view=CatalogTable/>
-                    <Route path="/tmflib/productCatalogManagement/category" view=CategoryTable/>
-                    <Route path="/tmflib/productCatalogManagement/productSpecification" view=ProductSpecificationTable/>
-                    <Route path="/tmflib/productCatalogManagement/productOffering" view=ProductOfferingTable/>
-                    <Route path="/tmflib/tmf629/customer" view=CustomerTable/>
-                    <Route path="/tmflib/tmf632/individual" view=IndividualTable/>
-                    <Route path="/tmflib/tmf632/organization" view=OrganizationTable>
-                        <Route path=":id" view=OrganizationView/>
-                    </Route>
                     <Route path="/*any" view=NotFound />
                 </Routes>
             </main>
