e5ed2ef946b2a3ab3770d1fbf359663fcacb2cbd
Ryan Ruckley <rruckley@gmail.com>
2024-01-22 11:30:04 +1100

    split common

---
+++ src/model/common/table.rs
@@ -0,0 +1,32 @@
+//! Common Table
+//! 
+//! 
+use leptos::*;
+use tmflib::{HasId,HasName};
+
+#[component]
+fn TableRow<T : HasId + HasName>(item : T) -> impl IntoView {
+    view! {
+        <tr><td><a href={item.get_href()}>{ item.get_name() }</a></td></tr>
+    }
+}
+
+#[component]
+pub fn GenericTable<T : HasId + HasName>(items : Vec<T>) -> impl IntoView {
+    view! {
+        <div class="gentable">
+            <table>
+                <tr>
+                    <th>{ T::get_class()}s</th>
+                </tr>
+         
+                {items.into_iter()
+                    .map(|c| {
+                        TableRow(TableRowProps{ item: c})
+                    }).collect_view()
+                }
+            </table>
+        </div>
+    }
+}
+
