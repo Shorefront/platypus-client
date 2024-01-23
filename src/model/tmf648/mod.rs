097178ed4674bb5fde2393f40d03cbc6056accd0
Ryan Ruckley <rruckley@gmail.com>
2024-01-24 10:28:37 +1100

    remove old table

--- src/model/tmf648/mod.rs
+++ src/model/tmf648/mod.rs
@@ -7,6 +7,8 @@ use leptos_router::*;
 use tmflib::HasId;
 use tmflib::tmf648::quote::Quote;
 
+use crate::model::common::form::{BasicClass,Validity,SingleRow};
+
 #[component]
 pub fn NoOptionView() -> impl IntoView {
     view! {
@@ -32,16 +34,10 @@ pub fn QuoteList() -> impl IntoView {
     let mut quote1 = Quote::new();
     quote1.id = Some("Quote123".to_string());
     let quotes = vec![quote1];
+    let add_href = format!("{}/add",Quote::get_class_href());
     view! {
         <div class="list">
-            <table>
-                <tr><th>"Quote Id"</th></tr>
-                {quotes.into_iter()
-                    .map(|q| {
-                        view! { <tr><td><a href={ q.get_href()}>{ q.id } </a></td></tr> }
-                    }).collect_view()
-                }
-            </table>
+            <a href=add_href>"Add"</a>
         </div>
         <div class="detail">
             <Outlet />
@@ -56,12 +52,38 @@ pub fn QuoteDetail() -> impl IntoView {
     }
 }
 
+#[component]
+pub fn BasicQuote(quote : Quote) -> impl IntoView {
+    let desc = quote.description.clone();
+    let cat = quote.category.clone();
+    view! {
+        <fieldset>
+            <legend>Quote Information</legend>
+            <BasicClass item=quote.clone()/>
+            <SingleRow id="description".to_string() label="Description".to_string() value=desc.unwrap_or_default() />
+            <SingleRow id="category".to_string() label="Category".to_string() value=cat.unwrap_or_default() />
+        </fieldset>
+    }
+}
+
+#[component]
+pub fn QuoteAdd() -> impl IntoView {
+    let quote =Quote::new();
+    let (name,set_name) = create_signal("New Quote".to_string());
+    view! {
+        <p>"Add Quote"</p>
+        <BasicQuote quote=quote.clone() />
+        <Validity item=quote />
+    }
+}
+
 #[component(transparent)]
 pub fn QuoteRoutes() -> impl IntoView {
     view! {
         <Route path="/tmf-api/tmf648/v4" view=QuoteHome>
             <Route path="quote" view=QuoteList >
                 <Route path=":id" view=QuoteDetail />
+                <Route path="add" view=QuoteAdd />
                 <Route path="" view=NoOptionView />
             </Route>
             <Route path="" view=NoOptionView />
