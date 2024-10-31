ca8f197610ecd61a574c5cf7f44e15262df6952f
Ryan Ruckley <rruckley@gmail.com>
2024-10-31 15:20:42 +1100

    new generic renderer

---
+++ src/model/common/list.rs
@@ -0,0 +1,37 @@
+//! List rendering
+//! 
+
+use leptos::*;
+use tmflib::{HasId,HasName};
+
+#[component]
+pub fn ListItem<T : HasId + HasName>(item : T) -> impl IntoView {
+    view! {
+        <li><a href={item.get_href()}>{ item.get_name() }</a></li>
+    }
+}
+
+#[component]
+pub fn GenericList<T : HasId + HasName>(items : Vec<T>) -> impl IntoView {
+    view! {
+        <h3>{ T::get_class()}s</h3>
+        <ul>
+            {
+                items.into_iter()
+                .map(|c| {
+                    ListItem(ListItemProps{ item: c})
+                }).collect_view()
+            }  
+        </ul>
+    }
+}
+
+#[component]
+pub fn GenericListWithAdd<T : HasId + HasName>(items : Vec<T>) -> impl IntoView {
+    let add_href = format!("{}/add",T::get_class_href());
+    let add_title = format!("Add {}",T::get_class());
+    view! {
+        <GenericList items = items />
+        <a href=add_href>{ add_title} </a>
+    }   
+}
