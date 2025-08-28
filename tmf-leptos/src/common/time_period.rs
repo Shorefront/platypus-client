6a9b819f199a6489524d0cde5a6c0be5869d8277
Ryan Ruckley <ryan.ruckley@optus.com.au>
2025-08-28 16:53:53 +1000

    feat: Enhance form for TimePeriod

--- tmf-leptos/src/common/time_period.rs
+++ tmf-leptos/src/common/time_period.rs
@@ -5,16 +5,41 @@ use tmflib::HasValidity;
 use leptos::prelude::*;
 
 #[component]
-pub fn TimePeriod<T : HasValidity,'a>(item : &'a T) -> impl IntoView {
+pub fn TimePeriod<T : HasValidity,'a>(item : &'a mut T) -> impl IntoView {
     let start = item.get_validity_start();
     let finish = item.get_validity_end();
+    let start_enabled = start.is_some();
+    let finish_enabled = finish.is_some();
     view!{
         <fieldset>
             <legend>Valid For</legend>
-            <label for="name">"Start:"</label>
-            <input type="text" id="start" name="start" value={start}/><br />
-            <label for="name">"Finish:"</label>
-            <input type="text" id="finish" name="finish" value={finish}/><br />
+            <label for="start">"Start"</label>
+            <input 
+                type="text" 
+                id="start" 
+                name="start" 
+                prop:value={start}
+            />
+            <input type="checkbox" checked={start_enabled}/>
+            <br />
+            <label for="finish">"Finish"</label>
+            <input 
+                type="text" 
+                id="finish" 
+                name="finish" 
+                value={finish}
+            />
+            <input type="checkbox" checked={finish_enabled}/>
+            <br />
+            <label for="presets">"Presets"</label>
+            <select id="presets" name="presets">
+                <option value="0">"Ongoing"</option>
+                <option value="7">"7 Days"</option>
+                <option value="30">"30 Days"</option>
+                <option value="90">"90 Days"</option>
+                <option value="365">"1 Year"</option>
+                <option value="-1">"For all time"</option>
+            </select>
         </fieldset>
     }
 }
