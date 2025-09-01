25e8dd1330dd1845c7dca824fa7c66e82de7e239
Ryan Ruckley <ryan.ruckley@optus.com.au>
2025-09-01 14:34:58 +1000

    fix: move to traits

--- client/src/model/common/form.rs
+++ client/src/model/common/form.rs
@@ -4,7 +4,9 @@ use leptos::prelude::*;
 use tmflib::{HasId, HasName, HasValidity};
 
 #[component]
-pub fn SingleRow(id: String, label: String, read: ReadSignal<String>, write: WriteSignal<String>) -> impl IntoView {
+pub fn SingleRow(id: impl Into<String>, label: impl Into<String>, read: ReadSignal<String>, write: WriteSignal<String>) -> impl IntoView {
+    let id = id.into();
+    let label = label.into();
     view! {
             <label for=id.clone()>{ label }</label>
             <input
