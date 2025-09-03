82379335c235e40c81417ff2376cf3a296a7b074
Ryan Ruckley <ryan.ruckley@optus.com.au>
2025-09-03 12:47:44 +1000

    doc: add TODO

--- tmf-leptos/src/common/last_update.rs
+++ tmf-leptos/src/common/last_update.rs
@@ -2,6 +2,7 @@
 use tmflib::HasLastUpdate;
 use leptos::prelude::*;
 
+/// TODO: use get_last_update() when tmflib updated to 0.1.34
 #[component]
 pub fn LastUpdate<T : HasLastUpdate,'a>(item: &'a T) -> impl IntoView {
     view! {
